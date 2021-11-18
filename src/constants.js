78ea4c9d35a2fe1dc231c69254e4b0b45485873f
Jia Ming <jma8774@bths.edu>
2021-11-17 19:02:12 -0500

    rip website

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
-// const WEBSITE_URL = 'https://url-shortener-fun.herokuapp.com/'
+const WEBSITE_URL = 'https://url-shortener-fun.herokuapp.com/'
 // const WEBSITE_URL = 'localhost:3000/'
 // const WEBSITE_URL = 'https://nocaplol.xyz/'
-const WEBSITE_URL = 'https://smallify.xyz/'
+// const WEBSITE_URL = 'https://smallify.xyz/'
 
 module.exports = {WEBSITE_URL};
